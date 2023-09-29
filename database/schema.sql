a5355f240f99f4887211339c4136da029140ff44
Tim Woo-Sam <twoosam10@gmail.com>
2023-09-29 23:16:41 +0000

    Initial commit.

--- database/schema.sql
+++ database/schema.sql
@@ -5,3 +5,37 @@ set client_min_messages to warning;
 drop schema "public" cascade;
 
 create schema "public";
+
+CREATE TABLE "user" (
+  "userId" serial PRIMARY KEY,
+  "username" text,
+  "hashedPassword" text,
+  "createdAt" timestamptz
+);
+
+CREATE TABLE "category" (
+  "categoryId" serial PRIMARY KEY,
+  "name" text,
+  "imageUrl" text
+);
+
+CREATE TABLE "product" (
+  "productId" serial PRIMARY KEY,
+  "name" text,
+  "details" text,
+  "price" integer,
+  "imageUrl" text,
+  "categoryId" integer
+);
+
+CREATE TABLE "cart" (
+  "userId" integer,
+  "productId" integer,
+  PRIMARY KEY ("userId", "productId")
+);
+
+ALTER TABLE "product" ADD FOREIGN KEY ("categoryId") REFERENCES "category" ("categoryId");
+
+ALTER TABLE "cart" ADD FOREIGN KEY ("userId") REFERENCES "user" ("userId");
+
+ALTER TABLE "cart" ADD FOREIGN KEY ("productId") REFERENCES "product" ("productId");
